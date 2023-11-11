0109674b51fd62660ca7f08e0835270e80fa5c1f
Gaelle327 <gaellesaid65@gmail.com>
2023-11-11 12:56:12 +0200

    eureka server up and working

--- src/main/java/com/fypgrading/eurekaService/ServiceRegistryApplication.java
+++ src/main/java/com/fypgrading/eurekaService/ServiceRegistryApplication.java
@@ -1,4 +1,4 @@
-package org.example;
+package com.fypgrading.eurekaService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
